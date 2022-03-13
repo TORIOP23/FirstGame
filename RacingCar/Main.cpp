b262dc6904173fc423c5e0b152ca3841b87c2240
TORIOP23 <vinhtran23022003@gmai.com>
2022-03-13 11:29:24 +0700

    Delay the window

--- RacingCar/Main.cpp
+++ RacingCar/Main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, SDL_WINDOW_SHOWN);
+	SDL_Delay(5000);
 	std::cout << "works";
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
