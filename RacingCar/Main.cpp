4a0874e264bdde769ee3c3acbe2ae82db412b1ed
TORIOP23 <vinhtran23022003@gmai.com>
2022-03-13 11:20:37 +0700

    First Commit

--- RacingCar/Main.cpp
+++ RacingCar/Main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, SDL_WINDOW_SHOWN);
 	std::cout << "works";
 	return 0;
 }
